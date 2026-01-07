0f4b8f7515f1e03ee17a926ebb3f29bec5817db5
Davi Freitas Rebouças <davifreitasreboucas.dev@gmail.com>
2026-01-07 17:38:55 -0300

    update infos

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const navLinks = document.getElementById('navLinks')
+const links = document.querySelectorAll('.links')
 const burguer = document.getElementById('burguer')
 
 burguer.addEventListener('click', () => {
     navLinks.classList.toggle('active')
+})
+
+links.forEach(link => { // função para fechar menu
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active')
+    })
 })
